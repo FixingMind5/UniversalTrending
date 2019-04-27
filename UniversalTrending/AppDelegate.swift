a1b58faa467c1ea3a07d271c74c35dfc77fa2ef5
FixingMind5 <aguilarmanu5@gmail.com>
2019-04-27 17:47:32 -0500

    Obtaining a tweet from twitter

--- UniversalTrending/AppDelegate.swift
+++ UniversalTrending/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        TWTRTwitter.sharedInstance().start(
+            withConsumerKey: "fQwJsGMKb9fv5S6mRPThIz24p",
+            consumerSecret: "ZuY5Aee88TZlZDay6YoDQf8doFDmJ4bl0IDuDj2okITfuTlvfy")
+        
         return true
     }
 
@@ -40,7 +45,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return TWTRTwitter.sharedInstance().application(app, open: url, options: options)
+    }
 
 }
 
---
+++ UniversalTrending/Main/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  UniversalTrending
+//
+//  Created by Manuel Alejandro Aguilar Tellez Giron on 4/27/19.
+//  Copyright © 2019 Manuel Alejandro Aguilar Tellez Giron. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        showTweet(id: "1122235471004880896")
+    }
+    
+    func showTweet(id: String) {
+        var tweetView: TWTRTweetView?
+        let client = TWTRAPIClient()
+        
+        
+        client.loadTweet(withID: id) { (tweet, error) in
+            if let t = tweet {
+                if let tweetView = tweetView {
+                    tweetView.configure(with: t)
+                    self.view.addSubview(tweetView)
+                } else {
+                    tweetView = TWTRTweetView(tweet: t, style: TWTRTweetViewStyle.regular)
+                    self.view.addSubview(tweetView!)
+                }
+            } else {
+                print("Failed to load tweet \(error!.localizedDescription)")
+            }
+        }
+    }
+    
+}
+
