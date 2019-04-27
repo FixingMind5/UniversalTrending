1ff9a1c673ac2a09769f2a3721aed81a98904fd3
FixingMind5 <aguilarmanu5@gmail.com>
2019-04-27 13:48:57 -0500

    Initial Commit

---
+++ UniversalTrending/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalTrending
+//
+//  Created by Manuel Alejandro Aguilar Tellez Giron on 4/27/19.
+//  Copyright © 2019 Manuel Alejandro Aguilar Tellez Giron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
