44b67f1e2f52201f0782e4cbda8a27fa9e506501
MACJaveloper <mac.javeloper@gmail.com>
2019-04-27 23:24:50 -0500

    Navegation v1.1

--- UniversalTrending/Main/GetTweet.swift
+++ UniversalTrending/Main/GetTweet.swift
@@ -16,6 +16,9 @@ class GetTweet: UITableViewController{
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellid, for: indexPath) as! TableViewCell
         //cell.textLabel?.text = "Something"
