0ce4aadbd79a3274dccd9b4767d73b0995ea6259
MACJaveloper <mac.javeloper@gmail.com>
2019-04-27 20:15:49 -0500

    Cambio del Segue

---
+++ UniversalTrending/Main/StatusChange.swift
@@ -0,0 +1,9 @@
+//
+//  StatusChange.swift
+//  UniversalTrending
+//
+//  Created by eyc on 27/04/19.
+//  Copyright © 2019 Manuel Alejandro Aguilar Tellez Giron. All rights reserved.
+//
+
+import Foundation
